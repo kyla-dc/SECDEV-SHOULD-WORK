3d5c2f871d51165de2bc6e8d2b9094d04e96a0eb
frncscantonio <francesca714.antonio@gmail.com>
2023-07-07 17:43:56 +0800

    implemented email, password, and username validation

--- controllers/signupController.js
+++ controllers/signupController.js
@@ -3,6 +3,9 @@ const User = require('../models/UserModel.js');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 const initID = 1000;
+var isValidEmail = false;
+var isValidPassword = false;
+var isValidUsername = false;
 
 const signupController = {
     getSignUp: function (req, res) {
@@ -21,50 +24,72 @@ const signupController = {
         var password = req.body.password;
         var phone = req.body.phone;
 
-        function insert(userID, result) {
-            userID = result.length + initID;
-            console.log(userID);
-            var user = {
-                userID: userID,
-                username: username,
-                password: password,
-                email: email,
-                firstName: firstName,
-                lastName: lastName,
-                phone: phone,
-                numPosts: 0,
-                followers: [],
-                avatar: '../images/default.png'
-            }
+        var regexEmail = /^(([a-zA-Z0-9])+(-|.|_)?([a-zA-Z0-9])+)+@([a-zA-Z0-9]|_)+(.\w{2})+$/;
+        var regexPass = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@!+"\/,_:#$%^&*()_\-=\[\]\{\};':"\\\|<>?])[a-zA-Z0-9@!+"\/,_:#$%^&*()_\-=\[\]\{\};':"\\\|<>?]{8,}/;
 
-            db.insertOne(User, user, function(result) {
-                if (result) {
-                    res.render('signup_success', {username: username});
-                }
-            });
+        /* checks if email format is valid */
+        if(!email.value.match(regexEmail)){
+            document.getElementById("wrong_email").style.display = "flex";
+        }
+        else {
+            document.getElementById("wrong_email").style.display = "none";
+            isValidEmail = true;
         }
 
-        bcrypt.hash(password, saltRounds, function (err, hash) {
-            password = hash;
-            var userID = 1000;
-            db.findMany(User, {}, 'userID', function(result) {
-                insert(userID, result);
-            });
-        });
-    },
+        /* checks if password strength is valid */
+        if(!password.value.match(regexPass)){
+            document.getElementById("invalid_password").style.display = "flex";
+        }
+        else{
+            document.getElementById("invalid_password").style.display = "none";
+            isValidPassword = true;
+        }
 
-    getCheckUsername: function (req, res) {
-        var username = req.query.username;
+        /* checks if username is unique */
         db.findOne(User, {username: username}, 'username', function (result) {
             res.send(result);
-        });
-    },
 
-    getCheckPassword: function (req, res) {
-        var password = req.query.password;
-        db.findOne(User, {password: password}, 'password', function (result) {
-            res.send(result);
+            if(result == null) {
+                document.getElementById("same_Username").style.display = "flex";
+            }
+            else {
+                document.getElementById("same_Username").style.display = "none";
+                isValidUsername = true;
+            }
         });
+
+        if(isValidEmail && isValidPassword && isValidUsername){
+            function insert(userID, result) {
+                userID = result.length + initID;
+                console.log(userID);
+                var user = {
+                    userID: userID,
+                    username: username,
+                    password: password,
+                    email: email,
+                    firstName: firstName,
+                    lastName: lastName,
+                    phone: phone,
+                    numPosts: 0,
+                    followers: [],
+                    avatar: '../images/default.png'
+                }
+    
+                db.insertOne(User, user, function(result) {
+                    if (result) {
+                        res.render('signup_success', {username: username});
+                    }
+                });
+            }
+    
+            bcrypt.hash(password, saltRounds, function (err, hash) {
+                password = hash;
+                var userID = 1000;
+                db.findMany(User, {}, 'userID', function(result) {
+                    insert(userID, result);
+                });
+            });
+        }
     }
 }
 
